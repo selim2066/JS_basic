c02fa44d66b096e894389374647f43ac8ec4d464
selim2066 <mdselimreza2066@gmail.com>
2025-05-08 11:04:43 +0600

    array learning

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
 console.log(2066)
-var id = 2066;
+let id = 2053;
 console.log(typeof id)
 console.log(id)
 var a ='55.7';
 var b= parseInt(a);
 console.log(b)
+
+for(i=0; i<2; i++)
+{
+    console.log("my name is Selim and id is " +id)
+}
+
+if(id==2066)
+{
+    console.log("Md Selim Reza ibn Abdul Latif")
+
+}else if(id==2053){
+    console.log("Md Rumon")
+
+
+}else{
+    console.log("user unidentify")
+}
+
+// shorthand
+const isPassed = false;
+if(!isPassed)
+{
+    console.log("fail tui")
+
+}else{
+    console.log("passed betttta")
+}
+
+// 
+// array task js
+// task:1
+let fruits =['am', 'jam', 'kola', 'lichu', 'tormuj'];
+console.log(fruits[2]);
+fruits[1]= 'jambura';
+console.log(fruits);
+
+// task:2
+let destination =['coxBazar', 'zaflong', 'tanguar haor']
+destination.push('peshoyar')
+destination.push('kashmir','kabul')
+destination.pop()
+console.log(destination)
+
+// task:3
