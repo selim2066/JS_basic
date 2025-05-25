c5855d63a3e3911aa8d0207cb9dc212163bc14a9
selim2066 <mdselimreza2066@gmail.com>
2025-05-25 14:53:11 +0600

    realistic age calculator done, alhamdulillah

---
+++ date.js
@@ -0,0 +1,32 @@
+/*const date = new Date();
+console.log(date);
+let y = date.getFullYear();
+let m = date.getMonth();
+let d = date.getDate();
+// console.log(year, +month, +day);
+
+let today = [y, m, d];
+console.log(today);
+*/
+// AGGE Calculator
+function age(year, month, day) {
+  const today = new Date();
+  let y = today.getFullYear() - year;
+  let m = today.getMonth() - month + 1;
+  let d = today.getDate() - day;
+
+  if (d < 0) {
+    d += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    m--;
+  }
+  if(m<0){
+    m+=12;
+    y--;
+  }
+  console.log(y + " years " + m + " month " + d + " days");
+}
+
+
+age(2000, 4, 15);
+age(2000, 11, 29);
+age(1996, 2, 5);
