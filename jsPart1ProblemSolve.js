3803549c5c7eb4c3f8436a5dadcc67c000ec2e6e
selim2066 <mdselimreza2066@gmail.com>
2025-05-29 20:00:41 +0600

    biggest string identify program using js

--- jsPart1ProblemSolve.js
+++ jsPart1ProblemSolve.js
@@ -6,8 +6,7 @@ function temperatureC(C, F) {
   console.log("fahrenheit to Celsius : " + F + " is equal to " + celsius);
   console.log("Celsius to Kelvin : " + C + " is equal to " + kelvin);
 }
-temperatureC(40,100)
-
+temperatureC(40, 100);
 
 /* Task-2:
 You are given an array of numbers. Count how many times the a number is repeated in the array.
@@ -15,35 +14,59 @@ You are given an array of numbers. Count how many times the a number is repeated
 sample-input: numbers = [5,6,11,12,98, 5]*/
 
 numbers = [5, 6, 11, 12, 98, 5];
-function repeated(nums){
-    let rep={}
-    for(let i of nums)
-    {
-      if(rep[i]){
-        rep[i]++;
-      }else{
-        rep[i]=1;
-      }
+function repeated(nums) {
+  let rep = {};
+  for (let i of nums) {
+    if (rep[i]) {
+      rep[i]++;
+    } else {
+      rep[i] = 1;
     }
-    console.log(rep)
+  }
+  console.log(rep);
 }
-repeated(numbers)
+repeated(numbers);
 
 /*
 ask-3:
 Write a function to count the number of vowels in a string.
 */
 
-function vowelsCount(text){
+function vowelsCount(text) {
   let vowels = ["a", "e", "i", "o", "u"];
   let count = 0;
-  for(let i of text)
-  {
-    if(vowels.includes(i)){
+  for (let i of text) {
+    if (vowels.includes(i)) {
       count++;
     }
   }
-  console.log("number of vowels in text is : \n"+count)
+  console.log("number of vowels in text is : \n" + count);
 }
-let txt="hi my name is selim"
-vowelsCount(txt)
+let txt = "hi my name is selim";
+vowelsCount(txt);
+
+/* Task-4:
+Write a function to find the longest word in a given string.
+*/
+
+let text = "hi my name is MdSelimRezaIbnAL";
+function longestString(txt) {
+  let tArray = txt.split(" ");
+  let biggest = 0;
+  let word;
+  for (word of tArray) {
+    let size = word.length;
+    if (size > biggest) {
+      biggest = size;
+    }
+  }
+  console.log(word + " is the biggest word of the string");
+}
+longestString(text);
+/* 
+sample-input: I am learning Programming to become a programmer
+
+sample-output: Programming
+
+Task-5:
+Generate a random number between 10 to 20.*/
